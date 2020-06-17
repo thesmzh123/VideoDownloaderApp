143c28942fdbc2fef10835663ca094a00ad78687
HP <thegame.mzh@gmail.com>
2020-06-17 17:23:33 +0500

    initial commit.

--- app/src/main/java/com/video/downloading/app/downloader/online/app/fragments/PasteLinkFragment.kt
+++ app/src/main/java/com/video/downloading/app/downloader/online/app/fragments/PasteLinkFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import com.google.android.material.textfield.TextInputEditText
 import com.video.downloading.app.downloader.online.app.R
 import kotlinx.android.synthetic.main.fragment_paste_link.view.*
@@ -18,6 +19,11 @@ class PasteLinkFragment : BaseFragment() {
         root = inflater.inflate(R.layout.fragment_paste_link, container, false)
         urlText = root!!.videoUrl
         root!!.downloadBtn.setOnClickListener {
+            if (urlText!!.text!!.isEmpty()) {
+                showToast(getString(R.string.fill_the_field))
+            } else if (!URLUtil.isValidUrl(urlText!!.text.toString())){
+                showToast(getString(R.string.valid_url))
+            }
         }
         return root!!
     }
