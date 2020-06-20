5b4bb5c5da1df9f81d61455e7d38e0377f777f78
HP <thegame.mzh@gmail.com>
2020-06-20 10:02:04 +0500

    initial commit.

--- app/src/main/java/com/video/downloading/app/downloader/online/app/fragments/DownloadFragment.kt
+++ app/src/main/java/com/video/downloading/app/downloader/online/app/fragments/DownloadFragment.kt
@@ -27,26 +27,30 @@ class DownloadFragment : BaseFragment() {
         root = inflater.inflate(R.layout.fragment_download, container, false)
         downloadFileList = ArrayList()
 
-        root1 = if (Build.VERSION.SDK_INT >= 29) {
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        try {
+            root1 = if (Build.VERSION.SDK_INT >= 29) {
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
 
-        } else {
+            } else {
 
-            Environment.getExternalStorageDirectory()
+                Environment.getExternalStorageDirectory()
 
-        }
-        val path = root1!!.absolutePath + "/" + DOWNLOAD_DIRECTORY + "/"
-        Log.d(TAGI, "Path: $path")
-        val directory = File(path)
-        val files = directory.listFiles()!!
-        Log.d(TAGI, "Size: " + files.size)
+            }
+            val path = root1!!.absolutePath + "/" + DOWNLOAD_DIRECTORY + "/"
+            Log.d(TAGI, "Path: $path")
+            val directory = File(path)
+            val files = directory.listFiles()!!
+            Log.d(TAGI, "Size: " + files.size)
 
-        for (file in files) {
-            Log.d(TAGI, "FileName:" + file.name)
-            val fileName = file.name
-            val recordingUri =
-                root1!!.absolutePath + "/" + DOWNLOAD_DIRECTORY + "/" + fileName
-            downloadFileList!!.add(DownloadFile(recordingUri, fileName))
+            for (file in files) {
+                Log.d(TAGI, "FileName:" + file.name)
+                val fileName = file.name
+                val recordingUri =
+                    root1!!.absolutePath + "/" + DOWNLOAD_DIRECTORY + "/" + fileName
+                downloadFileList!!.add(DownloadFile(recordingUri, fileName))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
         //creating our adapter
         val adapter =
